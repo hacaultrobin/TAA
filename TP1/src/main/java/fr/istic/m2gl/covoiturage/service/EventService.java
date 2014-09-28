24df98b9f91bc6277e89172e88bac014ae5b95f7
hacault.robin <hacault.robin@free.fr>
2014-09-28 16:26:49 +0200

    [TP1] - Modif interface EventService, avec JavaDoc

--- TP1/src/main/java/fr/istic/m2gl/covoiturage/service/EventService.java
+++ TP1/src/main/java/fr/istic/m2gl/covoiturage/service/EventService.java
@@ -1,15 +1,60 @@
 package fr.istic.m2gl.covoiturage.service;
 
+import java.util.Collection;
 import java.util.Date;
 
+import fr.istic.m2gl.covoiturage.domain.Car;
+import fr.istic.m2gl.covoiturage.domain.Event;
 import fr.istic.m2gl.covoiturage.domain.User;
 
 public interface EventService {
-
-	public void addEvent(int id, Date d, String lieu);
-	public void removeEvent();
 	
-	public void addUserAsParticipant(User p);
-	public void removeUserAsParticipant(User p);
+	/**
+	 * @param id is the event to get
+	 * @return the event designed by id
+	 */
+	public Event getEvent (int id);
+	
+	/**
+	 * @return a collection of the available events
+	 */
+	public Collection<Event> getEvents();
+
+	/**
+	 * Add a new event
+	 * @param d is the date of the event
+	 * @param lieu indicates where it takes place
+	 * @param desc is a short description of the event
+	 */
+	public void addEvent(Date d, String lieu, String desc);
+	
+	/**
+	 * Remove an event, all users leaves it
+	 * @param e is the event to remove
+	 */
+	public void removeEvent(Event e);	
+
+	
+	/**
+	 * Add passenger to the event e if cars are available
+	 * @param e is the event to join
+	 * @param passenger is the user to add to the event
+	 */
+	public void joinEvent (Event e, User passenger);
+	
+	/**
+	 * Add driver to the event e, who is the driver of the new car c
+	 * @param e is the event to join
+	 * @param driver is the user to add to the event and who will drive the new car c
+	 * @param c is the new car added to the event
+	 */
+	public void joinEventWithCar (Event e, User driver, Car c);
+	
+	/**
+	 * Delete user from e, and user's car if he's the driver 
+	 * @param e The event to leave
+	 * @param user is the participant (driver or passenger) who leave the event
+	 */
+	public void leaveEvent (Event e, User user);
 	
 }
