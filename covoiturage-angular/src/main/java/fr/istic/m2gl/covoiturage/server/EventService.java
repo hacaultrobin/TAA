5eed87c815eaa7f4dd9b88d3b6f33490319a7d14
hacaultrobin <hacault.robin@free.fr>
2014-10-29 18:43:54 +0100

    [Covoit] Recopie modifs serveur vers projet Angular (2/2)

---
+++ covoiturage-angular/src/main/java/fr/istic/m2gl/covoiturage/server/EventService.java
@@ -0,0 +1,78 @@
+package fr.istic.m2gl.covoiturage.server;
+
+import java.util.Collection;
+import java.util.Date;
+
+import javax.ws.rs.core.Response;
+
+import fr.istic.m2gl.covoiturage.db.Car;
+import fr.istic.m2gl.covoiturage.db.Event;
+import fr.istic.m2gl.covoiturage.db.User;
+
+public interface EventService {
+	
+	/**
+	 * @param id is the event to get
+	 * @return the event designed by id
+	 */
+	public Event getEvent (int id);
+	
+	/**
+	 * @param id is the event to get users
+	 * @return the users of the event designed by id
+	 */
+	public Collection<User> getEventUsers (int id);
+	
+	/**
+	 * @param id is the event to get cars
+	 * @return the cars of the event designed by id
+	 */
+	public Collection<Car> getEventCars (int id);
+	
+	/**
+	 * @return a collection of the available events
+	 */
+	public Collection<Event> getEvents();
+
+	/**
+	 * Add a new event
+	 * @param d is the date of the event
+	 * @param lieu indicates where it takes place
+	 * @param desc is a short description of the event
+	 * @return an HTTP Response, status 200 (added)
+	 */
+	public Response addEvent(Date d, String lieu, String desc);
+	
+	/**
+	 * Remove an event and the users/cars which participated
+	 * @param e is the event to remove
+	 * @return an HTTP Response, status 200 (removed) or 202 (not removed --> event already removed)
+	 */
+	public Response removeEvent(int idEvent);	
+	
+	/**
+	 * Create a new user and add him as a PASSENGER to the event if seats are available
+	 * @param idEvent is the id of the event to join
+	 * @param namePassenger is the name of the user to create and to add to the event as a passenger
+	 * @return an HTTP Response, status 200 (added) or 202 (not added -> no seats)
+	 */
+	public Response joinEvent (int idEvent, String namePassenger);	
+	
+	/**
+	 * Create a new user and a new car and add him as a DRIVER to the event, with his new car
+	 * @param idEvent is the id of the event to join
+	 * @param nameDriver is the name of the user to create and to add to the event as a driver
+	 * @param modelCar is the model of the car drived by nameDriver to create and to add to the event
+	 * @param nbSeatsCar is the nbSeats of the car
+	 * @return an HTTP Response, status 200 (added) or 202 (not added -> no seats)
+	 */
+	public Response joinEventWithCar (int idEvent, String nameDriver, String modelCar, Integer nbSeatsCar);
+	
+	/**
+	 * Delete user from e, and user's car if he's the driver 
+	 * @param e The event to leave
+	 * @param user is the participant (driver or passenger) who leave the event
+	 */
+	public Response leaveEvent (int idEvent, int idUser);
+	
+}
---
+++ covoiturage-angular/src/main/java/fr/istic/m2gl/covoiturage/server/UserResource.java
@@ -0,0 +1,150 @@
+package fr.istic.m2gl.covoiturage.server;
+
+import java.util.Collection;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import fr.istic.m2gl.covoiturage.db.Car;
+import fr.istic.m2gl.covoiturage.db.Event;
+import fr.istic.m2gl.covoiturage.db.User;
+
+@Path("/users")
+public class UserResource implements UserService {
+
+	EntityManager manager;
+
+	public UserResource() {
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("dev");
+		manager = factory.createEntityManager();
+	}
+
+	@GET
+	@Path("search/{id}")
+	@Produces({MediaType.APPLICATION_JSON})
+	public User getUser(@PathParam("id") int id) {
+		User user = manager.find(User.class, id);
+		return user;
+	}
+
+	@GET
+	@Produces({MediaType.APPLICATION_JSON})
+	public Collection<User> getUsers() {
+		@SuppressWarnings("unchecked")
+		Collection<User> users = manager.createQuery("select u from User u").getResultList();		
+		return users;
+	}
+
+	@POST
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public void addUser(@QueryParam("name") String name) {
+		User user = new User(name);
+		
+		EntityTransaction t = manager.getTransaction();
+		t.begin();
+		manager.persist(user);
+		t.commit();
+	}
+
+	@DELETE
+	@Path("delete/{id}")
+	public void removeUser(@PathParam("id") int id) {
+		User user = manager.find(User.class, id);
+		if (user != null) {
+			Event event = user.getEvent();
+			Car car = user.getCar();
+			
+			EntityTransaction t = manager.getTransaction();
+			t.begin();
+			if (event != null) {
+				event.removeParticipant(user);
+			}
+			if (car != null) {
+				if (user.isDriver()) {
+					car.setDriver(null);
+				} else {
+					car.removePassenger(user);
+				}
+			}
+			manager.remove(user);
+			t.commit();
+		}
+	}
+
+	@POST
+	@Path("event/")
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public void joinEvent(@QueryParam("idUser") int idUser, @QueryParam("idEvent") int idEvent) {
+		User user = manager.find(User.class, idUser);
+		Event event = manager.find(Event.class, idEvent);
+
+		if (user != null && event != null) {
+			EntityTransaction t = manager.getTransaction();
+			t.begin();
+			user.setEvent(event);
+			event.addParticipant(user);
+			t.commit();
+		}
+	}
+
+	@DELETE
+	@Path("event/delete/{id}")
+	public void leaveEvent(@PathParam("id") int idUser) {
+		User user = manager.find(User.class, idUser);
+		if (user != null) {
+			Event event = user.getEvent();
+
+			EntityTransaction t = manager.getTransaction();
+			t.begin();
+			if (event != null) { event.removeParticipant(user); }
+			user.setEvent(null);
+			t.commit();
+		}
+	}
+
+	@POST
+	@Path("car/")
+	@Consumes({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+	public void joinCar(@QueryParam("idUser") int idUser, @QueryParam("idCar") int idCar, @QueryParam("isDriver") boolean isDriver) {
+		User user = manager.find(User.class, idUser);
+		Car car = manager.find(Car.class, idCar);
+
+		if (user != null && car != null) {
+			EntityTransaction t = manager.getTransaction();
+			t.begin();
+			user.setCar(car);
+			if (isDriver) {
+				car.setDriver(user);
+			} else {
+				car.addPassenger(user);
+			}
+			t.commit();
+		}
+	}
+
+	@DELETE
+	@Path("car/delete/{id}")
+	public void leaveCar(@PathParam("id") int idUser) {
+		User user = manager.find(User.class, idUser);
+		if (user != null) {
+			Car car = user.getCar();
+
+			EntityTransaction t = manager.getTransaction();
+			t.begin();
+			if (car != null) { car.removePassenger(user); }
+			user.setCar(null);;
+			t.commit();
+		}
+	}
+}
